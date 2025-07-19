4de1bbe10a33331c6f0755254bbb11d34fe6cdce
nw117722nw1 <roos1100110022r@gmail.com>
2025-07-19 03:30:56 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,16 @@
-from setuptools import setup, find_packages
+
+from setuptools import setup
 
 setup(
-    name="secure-seed00",
-    version="1.0.0",
-    py_modules=['seed_generator'],
-    author="Nwal",
-    author_email="roos1100110022r@gmail.com",  # يمكنك وضع بريدك هنا
-    description="High-entropy 512-bit Seed Generator using 10 entropy sources for cryptography and PRNG.",
-    url="https://github.com/nw117722nw1/SecureSeed-Python2",
-    
-    install_requires=[
-        "psutil",  # مكتبة لقراءة معلومات النظام (CPU/RAM)
-    ],
-    python_requires=">=3.7",
+    name='secure-seed00',
+    version='0.1',
+    py_modules=['secure-seed00'],
+    author='man53d',
+    author_email='roos1100110022r@gmail.com',
+    description='Generate a strong random seed from multiple sources',
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
     ],
+    python_requires='>=3.8',
 )
-
